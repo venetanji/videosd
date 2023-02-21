0e70238d283541b2980065230245dfee900cdbb7
Giovanni Lion <giovanni.lion@gmail.com>
2023-02-21 14:55:13 +0800

    removing express app

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.static('public'))
-
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Running client on port ${port}`)
-})
